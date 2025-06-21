376e132a475467adf4f5d2c80d079f4c9b839701
huai-gif <17631583852@163.com>
2025-06-22 00:33:14 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QFontDatabase>
+
+int main(int argc, char *argv[])
+{
+    // 设置高DPI支持
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QApplication a(argc, argv);
+
+    // Qt 6 默认使用 UTF-8 编码，不需要手动设置
+    // QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
+    // 添加中文字体支持
+    QFont font("Microsoft YaHei", 9);  // 使用微软雅黑字体
+    a.setFont(font);
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
